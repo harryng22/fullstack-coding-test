a644368566427e71d4f09df043bc699a851eaeaf
Hieu Nguyen <hieu@email.com>
2021-12-01 01:16:02 +0700

    Draftjs

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -7,12 +7,21 @@ import {
     Box,
     Stack, Button
 } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
 // import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import dynamic from 'next/dynamic'
 import { EditorProps } from 'react-draft-wysiwyg'
+import axios from 'axios';
+import draftToHtml from 'draftjs-to-html';
+// import htmlToDraft from 'html-to-draftjs';
+import { convertFromHTML, ContentState, convertFromRaw, EditorState } from 'draft-js';
+
+let htmlToDraft = null;
+if (typeof window === 'object') {
+    htmlToDraft = require('html-to-draftjs').default;
+}
 
 // install @types/draft-js @types/react-draft-wysiwyg and @types/draft-js @types/react-draft-wysiwyg for types
 
@@ -22,55 +31,118 @@ const Editor = dynamic<EditorProps>(
 )
 
 const Add = () => {
-    const [editorState, setEditorState] = useState('');
+    // const [editorState, setEditorState] = useState('');
+    const [model, setModel] = useState({
+        title: '',
+        body: '',
+        imageUrl: '',
+        imageAlt: ''
+    });
+
+    const [contentState, setContentState] = useState(null);
+    const [editorState, setEditorState] = useState(null)
+
     const router = useRouter();
     const { id } = router.query;
-    if (id === 'create') {
 
+    useEffect(() => {
+        if (id === 'create') {
+            setModel({
+                title: '',
+                body: '',
+                imageUrl: '',
+                imageAlt: ''
+            })
+        } else {
+            axios.get('https://localhost:7077/blog/' + id)
+                .then(response => {
+                    if (response.data) {
+                        setModel(response.data);
+                        const blocksFromHTML = convertFromHTML(response.data.body)
+                        const content = ContentState.createFromBlockArray(blocksFromHTML)
+                        const editorValue = EditorState.createWithContent(content);
+                        setEditorState(editorValue);
+                    }
+                })
+                .catch(error => console.log(error));
+        }
+    }, [id])
+
+    const onEditorStateChange = (editorState) => {
+        setModel({ ...model, body: draftToHtml(editorState) });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('https://localhost:7077/blog', { ...model })
+            .then(response => {
+                if (response.data) {
+                    console.log(response.data);
+                }
+            })
+            .catch(error => console.log(error));
     }
+
+
     return (
         <Box backgroundColor='#f1f1f1' h='100vh' padding='10'>
-            <Stack
-                spacing={4}
-                p="1rem"
-                backgroundColor="whiteAlpha.900"
-                boxShadow="md"
-                borderRadius='5'
-                alignItems='center'
-            >
-                <FormControl id='title' isRequired>
-                    <FormLabel>Title: </FormLabel>
-                    <Input type='text' placeholder='Please enter title' />
-                </FormControl>
-                <FormControl id='body' isRequired h='300px'>
-                    <FormLabel>Body: </FormLabel>
-                    <Editor
-                        editorState={editorState}
-                        toolbarClassName="toolbarClassName"
-                        wrapperClassName="wrapperClassName"
-                        editorClassName="editorClassName"
-                        onEditorStateChange={setEditorState}
-                    />
-                </FormControl>
-                <FormControl id='imageUrl' isRequired>
-                    <FormLabel>Image URL: </FormLabel>
-                    <Input type='text' placeholder='Please enter image Url' />
-                </FormControl>
-                <FormControl id='imageAlt' isRequired>
-                    <FormLabel>Image Alt: </FormLabel>
-                    <Input type='text' placeholder='Please enter image Alt' />
-                </FormControl>
-                <Button
-                    borderRadius={5}
-                    type="submit"
-                    variant="solid"
-                    colorScheme="blue"
-                    width="100px"
-                // disabled={isInvalid}
+            <form onSubmit={handleSubmit}>
+                <Stack
+                    spacing={4}
+                    p="1rem"
+                    backgroundColor="whiteAlpha.900"
+                    boxShadow="md"
+                    borderRadius='5'
+                    alignItems='center'
                 >
-                    Save
-                </Button>
-            </Stack>
+                    <FormControl id='title' isRequired>
+                        <FormLabel>Title: </FormLabel>
+                        <Input
+                            type='text'
+                            placeholder='Please enter title'
+                            value={model.title}
+                            onChange={({ target }) => setModel({ ...model, title: target.value })} />
+                    </FormControl>
+                    <FormControl id='body' isRequired h='300px'>
+                        <FormLabel>Body: </FormLabel>
+                        {console.log(contentState)}
+                        <Editor
+                            // editorState={editorState}
+                            editorContent={contentState}
+                            toolbarClassName="toolbarClassName"
+                            wrapperClassName="wrapperClassName"
+                            editorClassName="editorClassName"
+                            onContentStateChange={onEditorStateChange}
+                        />
+                    </FormControl>
+                    <FormControl id='imageUrl' isRequired>
+                        <FormLabel>Image URL: </FormLabel>
+                        <Input
+                            type='text'
+                            placeholder='Please enter image Url'
+                            value={model.imageUrl}
+                            onChange={({ target }) => setModel({ ...model, imageUrl: target.value })} />
+                    </FormControl>
+                    <FormControl id='imageAlt' isRequired>
+                        <FormLabel>Image Alt: </FormLabel>
+                        <Input
+                            type='text'
+                            placeholder='Please enter image Alt'
+                            value={model.imageAlt}
+                            onChange={({ target }) => setModel({ ...model, imageAlt: target.value })} />
+                    </FormControl>
+                    <Button
+                        borderRadius={5}
+                        type="submit"
+                        variant="solid"
+                        colorScheme="blue"
+                        width="100px"
+                    // disabled={isInvalid}
+                    >
+                        Save
+                    </Button>
+                </Stack>
+            </form>
         </Box>
     )
 }
